d2b613fc3654f0a168180a3e90cdfcc3e3a45e1f
Le Hong Quan <quan.le@letsee.io>
2022-12-14 17:14:06 +0900

    feat: init commit for template.

---
+++ src/Experience/sources.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: "testMatcap",
+    type: "texture",
+    path: "assets/textures/matcap.png",
+  },
+  {
+    name: "foxModel",
+    type: "gltfModel",
+    path: "assets/models/Fox/glTF/Fox.gltf",
+  },
+];
